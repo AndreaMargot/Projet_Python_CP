00c143c26460c17627cb3d2e173d7e1fb7541ae9
Audrey Drieux <audrey.drieux@gmail.com>
2026-03-05 15:48:23 +0100

    version numba modifiée

--- corps_numba.py
+++ corps_numba.py
@@ -2,29 +2,34 @@ import numpy as np
 import numba
 
 
-def update_pos_v(position, vitesse, acceleration, delta_t):
-    position = position + delta_t*vitesse + 0.5*(delta_t)**2*acceleration
-    vitesse = vitesse + delta_t*acceleration
-    return position, vitesse
+def update_pos_v(positions, vitesses, accelerations, delta_t):
+    positions = positions + delta_t*vitesses + 0.5*(delta_t)**2*accelerations
+    vitesses = vitesses + delta_t*accelerations
+    return positions, vitesses
 
 
 @numba.njit(parallel=True)
-def force_attraction(masse, position): #renvoie l'accelération
-    n = len(masse)
-    f : np.ndarray = np.empty((n,3),dtype=np.float64) #array non initialisé
+def acc(masses, positions):
+    """
+    renvoie un tableau qui contient l'accélération de
+    chaque corps de la galaxie 
+    
+    """
+    nb_corps = len(masses)
+    a : np.ndarray = np.empty((nb_corps,3),dtype=np.float64) 
     G : numba.float64 = 1.560339*1e-13
-    #G : np.double = 1.560339*1e-13
 
-    for i in numba.prange(n):
-        force = np.zeros(3,dtype=np.float64)
-        for j in numba.prange(n):
+
+    for i in numba.prange(nb_corps):
+        a_i = np.zeros(3,dtype=np.float64)
+        for j in numba.prange(nb_corps):
            if j != i:
-                diff_x = position[j, 0] - position[i, 0]
-                diff_y = position[j, 1] - position[i, 1]
-                diff_z =  position[j, 2] - position[i, 2]
-                norm = diff_x**2 + diff_y**2 + diff_z**2
-                force[0] += (masse[j]*diff_x)/(norm**3)
-                force[1] += (masse[j]*diff_y)/(norm**3)
-                force[2] += (masse[j]*diff_z)/(norm**3)
-        f[i]=G*force
-    return f #np.ndarray
+                diff_x = positions[j, 0] - positions[i, 0]
+                diff_y = positions[j, 1] - positions[i, 1]
+                diff_z =  positions[j, 2] - positions[i, 2]
+                norm = np.sqrt(diff_x**2 + diff_y**2 + diff_z**2)
+                a_i[0] += (masses[j]*diff_x)/(norm**3)
+                a_i[1] += (masses[j]*diff_y)/(norm**3)
+                a_i[2] += (masses[j]*diff_z)/(norm**3)
+        a[i]=G*a_i
+    return a #np.ndarray
